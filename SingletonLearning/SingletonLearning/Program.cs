b08dafb6113be7d875dbd6eb6d035782dcf44087
mdcxi <hannahkawaii1611@gmail.com>
2022-07-11 01:08:44 +0700

    Singleton Learning

---
+++ SingletonLearning/SingletonLearning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+sealed class Singleton
+{
+    private Singleton () {} //client code khong the khoi tao object cua class 
+    
+    //khoi tao thanh vien static cua class Singleton
+    private static readonly Singleton _instance = new Singleton();
+    
+    //truy xuat tu client code de lay object cua class Singleton
+    public static Singleton Instance => _instance;
+    
+    private int _count = 0;
+    public void Increase() => _count++;
+    public int Count => _count;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Singleton Design Pattern";
+        Console.WriteLine($"Start: {Singleton.Instance.Count}");
+        Singleton.Instance.Increase();
+        Console.WriteLine($"First increment: {Singleton.Instance.Count}");
+        Singleton.Instance.Increase();
+        Console.WriteLine($"Second increment: {Singleton.Instance.Count}");
+        Singleton.Instance.Increase();
+        Console.WriteLine($"Third increment : {Singleton.Instance.Count}");
+        Console.ReadKey();
+    }
+}
