1d4ff328233ffc156dcd4f5715cf11ac39b0c250
mdcxi <hannahkawaii1611@gmail.com>
2022-07-10 19:51:25 +0700

    Class Learning

---
+++ ClassLearning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class @class
+{
+    public string myVariable = string.Empty;
+
+    public @class () {}
+
+    public void PrintSomething(int value1, int value2)
+    {
+        Console.WriteLine("Hello {0} , How are you? {1}", value1, value2);
+    }
+
+    public int AutoImplementedProperty { get; set; }
+
+    private int myValue;
+    public int GetInstance
+    {
+        get { return myValue; }
+        set { myValue = value;  }
+    }
+}
---
+++ ClassLearning/ProgramMain.cs
@@ -0,0 +1,11 @@
+namespace ClassLearning;
+
+public class ProgramMain
+{
+    static void Main()
+    {
+        int value1 = 16, value2 = 11;
+        @class _class = new @class();
+        _class.PrintSomething(value1, value2);
+    }
+}
